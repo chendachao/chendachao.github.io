23cf0fdf777fa7442545c849234bd6923677fb31
chendachao <chendachao@outlook.com>
2017-09-25 18:36:07 +0800

    refactor

--- rollup.config.js
+++ rollup.config.js
@@ -20,7 +20,7 @@ import cssnano from 'cssnano';
 export default {
   entry: 'src/scripts/main.js',
   dest: 'build/js/main.min.js',
-  format: 'iife',
+  format: 'umd',
   sourceMap: 'inline',
   plugins: [
     json(),

581b2b089e970ae9d7bf5afa52e1b0d360d660a4
chendachao <chendachao@outlook.com>
2019-05-17 17:54:20 +0800

    update wds

--- scripts/publish.js
+++ scripts/publish.js
@@ -4,5 +4,8 @@ ghpages.publish('dist', {
   branch: 'master',
   repo: 'https://github.com/chendachao/chendachao.github.io.git'
 }, function(err) {
+  if (err) {
+    console.error(err);
+  }
   console.log('publish done');
 });

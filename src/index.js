0f89267e8371b1692db544f9ed384bd15af26182
chendachao <chendachao@outlook.com>
2019-06-06 17:30:41 +0800

    add animation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './app/main.css';
 import './app/main.js';
 
-if ('serviceWorker' in navigator) {
+console.log('NODE_ENV', process.env.NODE_ENV);
+
+if (process.env.NODE_ENV !== 'development' && 'serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/service-worker.js').then(registration => {
       console.log('SW registered: ', registration);

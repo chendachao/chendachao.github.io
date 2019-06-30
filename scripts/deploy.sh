574b59b108a7c2ed9c2e6576bc867eaee31bb9e5
chendachao <chendachao@outlook.com>
2019-06-30 15:11:09 +0800

    rm d

--- scripts/deploy.sh
+++
@@ -1,7 +0,0 @@
-git checkout -B master
-git add -f dist -n
-git commit -am "Update website" -n
-git filter-branch -f --prune-empty --subdirectory-filter dist
-git push -f origin master -n
-git checkout -
-

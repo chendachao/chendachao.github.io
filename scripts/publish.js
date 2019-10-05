22539a265c6b28a4bbbfa27b8599b988ab14c911
chendachao <chendachao@outlook.com>
2019-10-05 18:56:21 +0800

    ci

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,9 +1,13 @@
 const ghpages = require('gh-pages');
 
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+
+console.log(123, GITHUB_TOKEN);
+
 ghpages.publish('dist', {
   branch: 'master',
   // repo: 'https://github.com/chendachao/chendachao.github.io.git',
-  repo: 'https://$GITHUB_TOKEN@github.com/chendachao/stone.git',
+  repo: `https://${GITHUB_TOKEN}@github.com/chendachao/stone.git`,
   user: {
     name: 'chendachao',
     email: 'chendachao@outlook.com'

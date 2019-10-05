fde20dd12535198f06712f2811bafc6ca33a2e2b
chendachao <chendachao@outlook.com>
2019-10-05 19:00:31 +0800

    ci

--- scripts/publish.js
+++ scripts/publish.js
@@ -7,7 +7,7 @@ console.log(123, GITHUB_TOKEN);
 ghpages.publish('dist', {
   branch: 'master',
   // repo: 'https://github.com/chendachao/chendachao.github.io.git',
-  repo: `https://${GITHUB_TOKEN}@github.com/chendachao/stone.git`,
+  repo: `https://${GITHUB_TOKEN}@github.com/chendachao.github.io.git`,
   user: {
     name: 'chendachao',
     email: 'chendachao@outlook.com'

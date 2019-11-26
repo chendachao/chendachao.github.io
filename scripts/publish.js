54ac6bb366c8b5a3f667ab8431ec7d2240192caf
chendachao <chendachao@outlook.com>
2019-11-26 18:59:21 +0800

    publish

--- scripts/publish.js
+++ scripts/publish.js
@@ -6,9 +6,12 @@ console.log(123, GITHUB_TOKEN);
 
 const repo = GITHUB_TOKEN ? `https://${GITHUB_TOKEN}@github.com/chendachao/chendachao.github.io.git` : 'https://github.com/chendachao/chendachao.github.io.git';
 
+console.log('repo', repo);
+
+
 ghpages.publish('dist', {
   branch: 'master',
-  repo: repo,
+  repo,
   user: {
     name: 'chendachao',
     email: 'chendachao@outlook.com'

e76e4151eff2ea9270c29b64a6146b3216ea91bf
chendachao <chendachao@outlook.com>
2019-11-27 17:20:25 +0800

    credential

--- scripts/hooks.sh
+++ scripts/hooks.sh
@@ -2,6 +2,8 @@
 
 git config --global user.name "chendachao"
 git config --global user.email "chendachao@outlook.com"
-git remote set-url origin 'https://${GITHUB_TOKEN}@github.com/chendachao/chendachao.github.io.git'
+# git remote rm origin
+# git remote add origin https://chendachao:${GITHUB_TOKEN}@github.com/chendachao/chendachao.github.io.git
+git remote set-url origin 'https://chendachao:${GITHUB_TOKEN}@github.com/chendachao/chendachao.github.io.git'
 
 

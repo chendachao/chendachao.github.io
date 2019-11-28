e35a4903b50fd6346c5ae038ff350a82490c4387
chendachao <chendachao@outlook.com>
2019-11-28 09:58:52 +0800

    c

--- scripts/hooks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-git config --global user.name "chendachao"
-git config --global user.email "chendachao@outlook.com"
-# git remote rm origin
-# git remote add origin https://chendachao:${GITHUB_TOKEN}@github.com/chendachao/chendachao.github.io.git
-git remote set-url origin 'https://chendachao:${GITHUB_TOKEN}@github.com/chendachao/chendachao.github.io.git'
-
-
--- scripts/publish.js
+++ scripts/publish.js
@@ -1,13 +1,7 @@
 const ghpages = require('gh-pages');
 
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
-
-console.log(123, GITHUB_TOKEN);
-
 const repo = GITHUB_TOKEN ? `https://chendachao:${GITHUB_TOKEN}@github.com/chendachao/chendachao.github.io.git` : 'https://github.com/chendachao/chendachao.github.io.git';
-
-console.log('repo', repo);
-
 
 ghpages.publish('dist', {
   branch: 'master',

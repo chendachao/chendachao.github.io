7c8854c71e02a9807e1acfbf0a9500e0c7e28306
chendachao <chendachao@outlook.com>
2019-11-28 11:44:45 +0800

    pusblish bugfix

--- scripts/publish.js
+++ scripts/publish.js
@@ -2,10 +2,15 @@ const ghpages = require('gh-pages');
 
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 const repo = GITHUB_TOKEN ? `https://chendachao:${GITHUB_TOKEN}@github.com/chendachao/chendachao.github.io.git` : 'https://github.com/chendachao/chendachao.github.io.git';
+const user = GITHUB_TOKEN ? {
+  name: 'chendachao',
+  email: 'chendachao@outlook.com'
+} : null;
 
 ghpages.publish('dist', {
   branch: 'master',
-  repo
+  repo,
+  user
 }, function(err) {
   if (err) {
     console.error(err);

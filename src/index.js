3ae07dc72c7b11d6b85eb5d52b86efecc2c64038
chendachao <chendachao@outlook.com>
2020-03-23 17:17:08 +0800

    pwa bugfix

--- src/index.js
+++ src/index.js
@@ -5,3 +5,13 @@ import(/* webpackPreload: true */'./print');
 import(/* webpackPreload: true */'./app/main.js');
 // import './app/main.js';
 
+if (process.env.NODE_ENV !== 'development' && 'serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').then(registration => {
+      console.log('SW registered: ', registration);
+    }).catch(registrationError => {
+      console.log('SW registration failed: ', registrationError);
+    });
+  });
+}
+

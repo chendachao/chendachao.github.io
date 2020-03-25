943b9bd44530805fddae3653ab4c2ff015409aae
chendachao <chendachao@outlook.com>
2020-03-25 22:42:49 +0800

    typo

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // import '@babel/polyfill';
-import './app/main.css';
 import(/* webpackPrefetch: true */'./pwa');
 import(/* webpackPreload: true */'./app/main.js');
+import './app/main.css';
 
 
 

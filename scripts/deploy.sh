c49ac3cff2721b1290409d2fc2dc4e59d98a03e5
Dachao Chen <dachao.cen@coupang.com>
2020-05-02 21:22:29 +0800

    refactor cicid

--- scripts/deploy.sh
+++
@@ -1,9 +0,0 @@
-cd git-lab
-git checkout -B specific
-git add -f dist -n
-git commit -am "Update website" -n
-git filter-branch -f --prune-empty --subdirectory-filter dist
-#git push -f origin specific -n
-git push -u -f origin specific
-git checkout master
-

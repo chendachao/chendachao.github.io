bb87618e0f71942a24dcbe34b238726f3af27acd
Dachao Chen <dachao.chen@coupang.com>
2021-01-18 20:52:09 +0800

    fix: version

--- src/error-tracing.js
+++ src/error-tracing.js
@@ -4,7 +4,7 @@ import { Integrations } from "@sentry/tracing";
 Sentry.init({
   dsn: 'https://d5fa791beeda48fcaa141dc31eaa8d8f@o175652.ingest.sentry.io/5596864',
   // To set your release version
-  release: "chendachao.github.io@" + process.env.npm_package_version,
+  release: "chendachao.github.io@" + process.env.VERSION,
   integrations: [
     new Integrations.BrowserTracing(),
   ],

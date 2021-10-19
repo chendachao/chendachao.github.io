de0b3afea2d643431212a8a01338cebfb0e31228
Dachao Chen <dachao.chen@coupang.com>
2021-10-19 13:28:55 +0800

    fix(i18n): add coffee

--- src/log.js
+++ src/log.js
@@ -1,1 +1,2 @@
-console.log('%c✨Hello, nice to meet you 🌹. This is Larry from China. Welcome to my home page.✨', 'padding: 0.3rem 1.5rem; font-family: Roboto; font-size: 1.2em; line-height: 1.4em; color: white; background: rgb(131,58,180); background: linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%); border-radius: 8px;');
+console.log('%c✨Hello, nice to meet you 💐. This is Larry from China. Welcome to my home page.✨', 'padding: 0.3rem 1.5rem; font-family: Roboto; font-size: 1.2em; line-height: 1.4em; color: white; background: rgb(131,58,180); background: linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%); border-radius: 8px;');
+// 🌹

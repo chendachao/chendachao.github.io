3e08c1f363eb1f968af1e9e500adb02ebee40e01
Dachao Chen <dachao.chen@coupang.com>
2022-05-04 13:47:37 +0800

    perf: page speed

--- src/assets/hotjar.js
+++ src/assets/hotjar.js
@@ -7,7 +7,8 @@
   h._hjSettings = { hjid: 1939682, hjsv: 6 };
   a = o.getElementsByTagName('head')[0];
   r = o.createElement('script');
-  r.async = 1;
+  // r.async = 1;
+  r.defer = 1;
   r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
   a.appendChild(r);
 })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');

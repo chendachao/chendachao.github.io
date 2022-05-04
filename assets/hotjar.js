050689d7fb68d92c2c7669b3ad24ed68309550f8
chendachao <chendachao@outlook.com>
2022-05-04 05:50:38 +0000

    Updates

--- assets/hotjar.js
+++ assets/hotjar.js
@@ -1,3 +1,3 @@
-(function(t,s,n,h,j,e){t.hj=t.hj||function(){(t.hj.q=t.hj.q||[]).push(arguments)},t._hjSettings={hjid:1939682,hjsv:6},j=s.getElementsByTagName("head")[0],e=s.createElement("script"),e.async=1,e.src=n+t._hjSettings.hjid+h+t._hjSettings.hjsv,j.appendChild(e)})(window,document,"https://static.hotjar.com/c/hotjar-",".js?sv=");
+(function(t,j,n,h,s,e){t.hj=t.hj||function(){(t.hj.q=t.hj.q||[]).push(arguments)},t._hjSettings={hjid:1939682,hjsv:6},s=j.getElementsByTagName("head")[0],e=j.createElement("script"),e.defer=1,e.src=n+t._hjSettings.hjid+h+t._hjSettings.hjsv,s.appendChild(e)})(window,document,"https://static.hotjar.com/c/hotjar-",".js?sv=");
 
 //# sourceMappingURL=hotjar.js.map
